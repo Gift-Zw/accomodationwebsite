9cb6194c00f4e0555bef88a1b45f04fb9f31955d
Gift-Zw <giftmugaragumbojr@gmail.com>
2022-03-27 14:50:40 +0200

    My first version to be deployed

--- accomodation/settings.py
+++ accomodation/settings.py
@@ -83,15 +83,25 @@ WSGI_APPLICATION = 'accomodation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+"""
+import django_heroku
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+from decouple import config
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
